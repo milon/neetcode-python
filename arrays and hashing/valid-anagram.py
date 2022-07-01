153942ad4d4b33e3b27aa78e13a92635ea3199ab
Nuruzzaman Milon <contact@milon.im>
2022-07-01 11:13:20 +0200

    added valid anagram

---
+++ arrays and hashing/valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    URL: https://leetcode.com/problems/valid-anagram/
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    Hint: Sort the strings and compare them.
+    """
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)): return False
+        
+        s = list(s)
+        t = list(t)
+
+        s.sort()
+        t.sort()        
+        
+        for i in range(len(s)):
+            if s[i] != t[i]: return False
+            
+        return True
