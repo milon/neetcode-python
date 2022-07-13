5d5bdfa2b8b99026d219fd10ad4e35558266ed41
Nuruzzaman Milon <contact@milon.im>
2022-07-13 23:14:27 +0200

    added single number

---
+++ 18. bit manupulation/single-number.py
@@ -0,0 +1,13 @@
+from typing import List
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
