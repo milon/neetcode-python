d26f304292e74aab075e2b57cbcc51a88e225626
Nuruzzaman Milon <contact@milon.im>
2022-07-13 17:16:51 +0200

    added valid sudoku

---
+++ arrays and hashing/valid-sudoku.py
@@ -0,0 +1,27 @@
+import collections
+from typing import List
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(1), since the grid is alyways a 9X9 square
+"""
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        sqrs = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                        board[r][c] in sqrs[(r//3, c//3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                sqrs[(r//3, c//3)].add(board[r][c])
+
+        return True
