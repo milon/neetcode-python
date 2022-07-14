97765dc4bedfa37cfe69a97199caf042b6b58dd2
Nuruzzaman Milon <contact@milon.im>
2022-07-14 10:08:44 +0200

    added diameter of binary tree

---
+++ 07. trees/diameter-of-binary-tree.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.answer = 0
+
+        def height(root):
+            if not root:
+                return 0
+            left, right = height(root.left), height(root.right)
+            self.answer = max(self.answer, left+right)
+            return 1 + max(left, right)
+
+        height(root)
+        return self.answer
