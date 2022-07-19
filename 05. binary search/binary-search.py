5a02757acbeeca1f1b616c8bb1c8c0626af45c2d
Nuruzzaman Milon <contact@milon.im>
2022-07-19 13:50:36 +0200

    added binary search

---
+++ 05. binary search/binary-search.py
@@ -0,0 +1,19 @@
+from typing import List
+
+"""
+Time Complexity: O(log(n))
+Space Complexity: O(1)
+"""
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        while l <= r:
+            m = (l+r)//2
+            if nums[m] < target:
+                l = m+1
+            elif nums[m] > target:
+                r = m-1
+            else:
+                return m
+        return -1
