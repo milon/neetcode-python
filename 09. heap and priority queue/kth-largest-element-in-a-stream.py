ea6678247cec76efeda4ece583af0fdf15b50221
Nuruzzaman Milon <contact@milon.im>
2022-07-20 12:18:08 +0200

    added kth-largest-element-in-a-stream

---
+++ 09. heap and priority queue/kth-largest-element-in-a-stream.py
@@ -0,0 +1,25 @@
+import heapq
+from typing import List
+
+"""
+Time Complexity: O(1) for each operation
+Space Complexity: O(k) for k is the number of elements in the heap
+"""
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
